424cb465b02f0ef4c57be54c57701bfd4659cb87
srkiyengar <srk.iyengar@gmail.com>
2017-08-21 21:01:48 -0400

    -Real time can read the tool pose (x,y,z,Rx,Ry,Rz).

--- src/tcp_client.py
+++ src/tcp_client.py
@@ -8,6 +8,8 @@ import time
 
 
 LOG_LEVEL = logging.DEBUG
+PORT_SECONDARY_CLIENT = 30002
+PORT_REALTIME_CLIENT = 30003
 
 # Set up a logger with output level set to debug; Add the handler to the logger
 my_logger = logging.getLogger("UR5_Logger")
@@ -17,7 +19,12 @@ class make_connection:
 
     def __init__(self, sock=None):
         if sock is None:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            except socket.error as e:
+                my_logger.info("Socket Error: {} ".format(e))
+                raise Exception('Could not create socket\n')
+
             self.sock.settimeout(10)
         else:
             self.sock = sock
@@ -29,8 +36,9 @@ class make_connection:
             self.socket_host = host
             self.socket_port = port
         except socket.timeout:
-            my_logger.info("Socket time out error")
+            my_logger.info("Socket Timeout: No socket connection to Host {} Port {}".format(host,port))
             self.link = 0
+            raise RuntimeError("Failure of socket connection to Host {} Port {}".format(host,port))
 
     def end_socket(self):
         #self.sock.shutdown(self.sock)
@@ -39,7 +47,7 @@ class make_connection:
     def send_data(self, msg):
 
         if(self.link == 0):
-            my_logger.info("Socket does not have an establsihed connection to a host/port")
+            my_logger.info("Socket does not have an established connection to a host/port")
             return 0
         message_len = len(msg)
         total = 0
@@ -48,8 +56,9 @@ class make_connection:
             if sent == 0:
                 raise RuntimeError("socket connection broken")
             total = total + sent
-        my_logger.info("Socket write - Length {} sent through socket to host - {}/port - {}",
-                       total,self.socket_host,self.socket_port)
+        my_logger.info("Socket write: {} \nLength {} sent through socket to host - {} and port - {}".
+                       format(msg, total,self.socket_host,self.socket_port))
+        return message_len
 
     def receive_data(self,how_many):
 
@@ -66,20 +75,33 @@ class make_connection:
             bytes_remaining = how_many-bytes_recd
         return received_data
 
-class ur5_connection:
+class ur5_connector:
 
     def __init__(self, host, port):
 
-        self.my_socket = make_connection()
-        self.my_socket.connect(host,port)
+        if port == PORT_REALTIME_CLIENT:
+            self.type = 1
+        elif port == PORT_SECONDARY_CLIENT:
+            self.type = 2
+        else:
+            self.type = 0
 
+        if (self.type != 0):
+            self.my_socket = make_connection()
+            self.my_socket.connect(host,port)
 
     def recv(self,no_bytes):
         return self.my_socket.receive_data(no_bytes)
 
+    def send(self,my_str):
+        return self.my_socket.send_data(my_str)
 
-    def close_ur5_connection(self):
+
+
+    def close(self):
         self.my_socket.end_socket()
+
+
 
 
 
--- src/ur5_interface.py
+++ src/ur5_interface.py
@@ -2,17 +2,79 @@ __author__ = 'srkiyengar'
 import logging
 import logging.handlers
 from datetime import datetime
-import tcp_client as tp
+import tcp_client as tc
 import time
 import struct
-import codecs
+import math
+
+
 
 #logger
 LOG_LEVEL = logging.DEBUG
 LOG_FILENAME = 'ur5' + datetime.now().strftime('%Y-%m-%d---%H:%M:%S')
 
-PORT_30003 = 30003
+PORT_SECONDARY_CLIENT = 30002
+PORT_REALTIME_CLIENT = 30003
 HOST = '192.168.10.2'
+
+# get_UR5_tool_position() will open and close the socket everytime a get request is made.
+
+def set_UR5_tool_position(tool_pose):
+    sc_connection = tc.ur5_connector(HOST,PORT_SECONDARY_CLIENT)
+    my_logger.info("Sending command to UR5")
+
+    #tool_str = str(list(tool_pose))
+    tool_str = tool_pose
+    acceleration = (math.pi*4)/9
+    velocity = math.pi/3
+    command_str = 'movej(' + tool_str + ',' + 'a=' + str(acceleration) + ',v='+ str(velocity) +')\n'
+    sc_connection.send(command_str)
+    sc_connection.close()
+    my_logger.info("Command Sent")
+
+def morph_tool_position(my_position,flip):
+
+    xyz_str = list(my_position[0:3])
+    if flip == 1:
+        Rxyz_str = [1.7380775, 0.65479857, -1.8207649]      #tcp 90 z-axis of tcp
+    else:
+        Rxyz_str = [-0.7586015, -1.679155, 1.778225]       #std
+    return (xyz_str + Rxyz_str)
+
+def get_UR5_tool_position():
+    rt_connection = tc.ur5_connector(HOST,PORT_REALTIME_CLIENT)
+    my_logger.info("reading packets from UR5")
+    message_size = rt_connection.recv(4)                    # Read first 4 bytes only
+    total_length = struct.unpack('!I',message_size)[0]      # Total length of the packets following the first 4 bytes
+    my_logger.info("Message Size of RT_client response from UR5: {}".format(total_length))
+    message = rt_connection.recv(total_length)
+    message_len = len(message)
+    rt_connection.close()
+    if message_len != total_length:
+        my_logger.info("Warning !!!! - {} Bytes read NOT equal to {} bytes expected from UR5".format(total_length,message_len))
+
+    tool_pos = message[440:488]                         # 48 bytes consisting of 8 bytes of x,y,z,rx,ry,rz)
+    return(struct.unpack('!dddddd',tool_pos))         # A tuple (x,y,z,rx,ry,rz)
+
+
+
+class UR5_commander:
+
+    def __init__(self,host=HOST):
+        self.commander = tc.ur5_connector(HOST,PORT_SECONDARY_CLIENT)
+        if self.commander.my_socket.link == 0:
+            raise RuntimeError("No socket connection to UR5")
+            self.connection = 0
+        else:
+            self.connection = 1
+        return
+
+    def send(self,command_str):
+        self.commander.send(command_str)
+
+    def close(self):
+        self.commander.close()
+
 
 if __name__ == '__main__':
 
@@ -25,39 +87,34 @@ if __name__ == '__main__':
     my_logger.addHandler(handler)
     # end of logfile preparation Log levels are debug, info, warn, error, critical
 
+    x,y,z,rx,ry,rz = get_UR5_tool_position()
 
+    acceleration = (math.pi*4)/9
+    velocity = math.pi/3
 
-    s = tp.ur5_connection(HOST, PORT_30003)
+    z = z + 0.20
+
+    pose_str = 'p['+ str(x)+','+ str(y)+','+ str(z)+','+ str(rx)+','+ str(ry)+','+ str(rz)+']' #To strip spaces
+
+    command_str = 'movej(' + pose_str + ',' + 'a=' + str(acceleration) + ',v='+ str(velocity) +')\n'
+    #command_str = 'set_digital_out(0,TRUE)\n'
+
+    remote_commander = UR5_commander(HOST)
+    if (remote_commander.connection == 1):
+        my_logger.info("Sending Command: {}".format(command_str))
+        remote_commander.send(command_str)
+    else:
+        my_logger.info("No link to UR5 to send Command: {}".format(command_str))
+
+    remote_commander.close()
+    #h = set_UR5_tool_position(g)
+
+    g = get_UR5_tool_position()
+
     time.sleep(1.00)
-
-    my_logger.info("reading packets from UR5")
-    packet_1 = s.recv(4)                                # Read first 4 bytes
-
-    total_length = struct.unpack('!I',packet_1)[0]      # Total length of the packets following the first 4 bytes
-    packet_2 = s.recv(total_length)
-    packet_len = len(packet_2)
-    if packet_len != total_length:
-        my_logger.info("Warning !!!! - Should have read {} bytes but got only {} bytes from UR5",)
-
-    tool_pos = packet_2[440:488]                        # 48 bytes consisting of 8 bytes of x,y,z,rx,ry,rz)
-    x,y,z,Rx,Ry,Rz = struct.unpack('!dddddd',tool_pos)
-
-    s.close_ur5_connection()
-
-    s = tp.ur5_connection(HOST, PORT_30003)
-    my_logger.info("reading packets from UR5")
-    packet_1 = s.recv(4)                                # Read first 4 bytes
-
-    total_length = struct.unpack('!I',packet_1)[0]      # Total length of the packets following the first 4 bytes
-    packet_2 = s.recv(total_length)
-    packet_len = len(packet_2)
-    if packet_len != total_length:
-        my_logger.info("Warning !!!! - Should have read {} bytes but got only {} bytes from UR5",)
-
-    tool_pos = packet_2[440:488]                        # 48 bytes consisting of 8 bytes of x,y,z,rx,ry,rz)
-    x1,y1,z1,Rx1,Ry1,Rz1 = struct.unpack('!dddddd',tool_pos)
-
-
-    s.close_ur5_connection()
-
     my_logger.info("Completed")
+
+
+
+
+
